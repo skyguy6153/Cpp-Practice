3b2afedd3dff8d008a890a89611625dbc201cdf3
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2024-05-25 19:22:47 -0400

    Add files via upload

---
+++ addInte/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int begin;
+    int end;
+    int sum;
+
+    begin = 34;
+    end = 99;
+    sum = begin + end;
+
+    cout << sum << endl;
+
+    return 0;
+}
